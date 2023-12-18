886119bb117402c7fa4c0b1995cfd0d74dc12417
n4ze3m <n4ze3m@gmail.com>
2023-12-18 11:09:54 +0530

    Update subject line for Secret Santa email

--- app/api/member/assign/route.ts
+++ app/api/member/assign/route.ts
@@ -43,7 +43,7 @@ export async function POST(request: NextRequest) {
       await resend.emails.send({
         from: "Secret Gifter 🎁 <gifter@hello.wayto.website>",
         to: [assignment.giver.email],
-        subject: "You are Secret Santa!",
+        subject: "It's time for some Secret Gifting!",
         html,
       });
     }
